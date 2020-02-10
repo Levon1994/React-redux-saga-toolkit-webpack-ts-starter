1d757baa45e85881f9aa91816f703ebb495cab42
Світлана Шарун <svitlanasarun@MacBook-Pro-Svitlana.local>
2020-02-10 18:04:33 +0200

    mcc012::add splash screen for ios

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
+import SplashScreen from 'react-native-splash-screen';
 
 import { theme } from './src/global-styled/theme';
 import { AppContainer } from './src/router';
 
 // eslint-disable-next-line no-console
 console.disableYellowBox = true;
-export const App: React.FC = () => (
-  <ThemeProvider theme={theme}>
-    <AppContainer />
-  </ThemeProvider>
-);
+export const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <AppContainer />
+    </ThemeProvider>
+  );
+};
